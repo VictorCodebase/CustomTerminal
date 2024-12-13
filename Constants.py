1aebf15049f6dd3388a6ec9d28edac46032326d2
victorcodebase <victorkithinji@outlook.com>
2024-12-13 09:33:26 +0300

    Irrelevant costants pruned

--- Constants.py
+++ Constants.py
@@ -2,44 +2,10 @@
 END_OF_FILE = 0xFF
 MAX_ARG = 99
 
-COLOR_MAP_16COLORS = {
-    "black": 0x00, "red": 0x01, "green": 0x02, "yellow": 0x03,
-    "blue": 0x04, "magenta": 0x05, "cyan": 0x06, "white": 0x07,
-    "bright_black": 0x08, "bright_red": 0x09, "bright_green": 0x0A,
-    "bright_yellow": 0x0B, "bright_blue": 0x0C, "bright_magenta": 0x0D,
-    "bright_cyan": 0x0E, "bright_white": 0x0F
-}
-
-COLOR_MAP_MONO = {
-    "black": 0x00, "white": 0x01
-}
-
-COLOR_MODE_MAPS = {
-    "monochrome" : {"black": 0x00, "white": 0x01 },
-    "16colors" : {
-    "black": 0x00, "red": 0x01, "green": 0x02, "yellow": 0x03,
-    "blue": 0x04, "magenta": 0x05, "cyan": 0x06, "white": 0x07,
-    "bright_black": 0x08, "bright_red": 0x09, "bright_green": 0x0A,
-    "bright_yellow": 0x0B, "bright_blue": 0x0C, "bright_magenta": 0x0D,
-    "bright_cyan": 0x0E, "bright_white": 0x0F,
-    },
-    "256colors":""
-}
-
-
 '''
 Map of all acceptable commands and their corresponding lengths
 <hex command> <hex legth>
 '''
-COMMANDS_AND_THEIR_LENGTHS = {
-    0x01: 3,
-    0x02: 4,
-    0x03: 6,
-    0x04: 3,
-    0x05: 2,
-    0x06: 2,
-    0x07: 1,
-}
 
 COMMANDS = {
     "screen_setup": {
